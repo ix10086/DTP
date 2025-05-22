2f541e515a42b5b28e1bb54a3da1b0d69f5af124
ix10086 <24643@burnside.school.nz>
2025-05-23 08:54:39 +1200

    111

--- dtptest.py
+++ dtptest.py
@@ -9,7 +9,6 @@ def print_all_cpu():
     sql = 'SELECT * FROM cpu'
     cursor.execute(sql)
     results = cursor.fetchall()
-    print(f'name                           ')
     for cpu in results:
         pprint(f'{cpu[1]:<6}{cpu[2]:<6}{cpu[3]:<6}{cpu[4]:<6}{cpu[5]:<6}{cpu[6]:<6}{cpu[7]:<6}{cpu[8]:<6}{cpu[9]:<15}')
     db.close()
